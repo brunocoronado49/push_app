ea741a8f41545feac6f15472dbc124c0e6c8a0e9
Francisco Rangel <francorangelcoronado@gmail.com>
2024-10-26 11:23:32 -0600

    Creando nuevo proyecto de notificaciones, configuraciones iniciales

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'config/theme/app_theme.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme().getTheme(),
+      home: const Scaffold(
+        body: Center(
+          child: Text('Hello Flutter!'),
+        ),
+      ),
+    );
+  }
+}
+
+
