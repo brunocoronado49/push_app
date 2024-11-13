763e54e599e8d47177094d0b9b03de52b1f280c3
brunocoronado49 <francorangelcoronado@gmail.com>
2024-11-13 00:11:34 -0600

    add pods with pod install

--- lib/presentation/blocs/notifications_bloc/notifications_bloc.dart
+++ lib/presentation/blocs/notifications_bloc/notifications_bloc.dart
@@ -63,8 +63,8 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
 
   void _getFCMToken() async {
     if(state.status != AuthorizationStatus.authorized) return;
-    // final token = await messaging.getToken();
-    // print('Token: $token');
+    final token = await messaging.getToken();
+    print('Token: $token');
   }
 
   void handleRemoteMessage(RemoteMessage message) {
